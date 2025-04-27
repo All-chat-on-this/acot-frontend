5ba63e6af20166ab13d0809d1eeecccb7d9460d5
MilkWind <2250957906@qq.com>
2025-04-28 01:51:35 +0800

    update: 更新网站信息

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import {resolve} from 'path'
+import {createHtmlPlugin} from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -8,7 +9,16 @@ export default defineConfig(({mode}) => {
     const env = loadEnv(mode, process.cwd())
 
     return {
-        plugins: [react()],
+        plugins: [
+            react(),
+            createHtmlPlugin({
+                inject: {
+                    data: {
+                        title: 'All-chat-on-this',
+                    },
+                },
+            }),
+        ],
         resolve: {
             alias: {
                 '@': resolve(__dirname, 'src')
